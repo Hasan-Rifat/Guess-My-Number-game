147861b22917cb4a3ac2402543884feeb798256e
Hasan-Rifat <developerhr000.com>
2022-11-02 17:26:11 +0600

    complete

--- script.js
+++ script.js
@@ -9,27 +9,56 @@ const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const highScore = document.querySelector('.highscore');
 
+let newScore = 20;
+let newHighScore = 0;
+
+// message function
+const messageHandler = function (message) {
+  document.querySelector('.message').innerText = message;
+};
+
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
-number.innerText = '?';
 
 checkBtn.addEventListener('click', () => {
   const checkNumber = parseInt(guess.value);
-  number.innerText = '?';
 
+  // empty value
   if (!checkNumber) {
-    message.innerText = '⛔ ️ No number!';
-  } else if (checkNumber === randomNumber) {
+    messageHandler('⛔ ️ No number!');
+  }
+  // current number
+  else if (checkNumber === randomNumber) {
     document.body.style.backgroundColor = '#60b347';
     number.innerText = randomNumber;
-    message.innerText = '🎉 Correct Number!';
-  } else if (checkNumber > randomNumber) {
-    message.innerText = '📈 to high';
-  } else if (checkNumber < randomNumber) {
-    message.innerText = '📈 to low';
+    messageHandler('🎉 Correct Number!');
+
+    //
+    if (newHighScore < newScore) {
+      newHighScore = newScore;
+      highScore.innerText = newHighScore;
+    }
+  }
+  // not equals
+  else if (checkNumber !== randomNumber) {
+    if (newScore > 0) {
+      // score
+      newScore--;
+      score.innerText = newScore;
+
+      //highscore
+      highScore.innerText = newHighScore;
+      checkNumber > randomNumber
+        ? messageHandler('📈 to high')
+        : messageHandler('📈 to low');
+    }
   }
 });
+
+// again btn
 again.addEventListener('click', () => {
   number.innerText = '?';
   guess.value = '';
   document.body.style.backgroundColor = '#222';
+  newScore = 20;
+  score.innerText = newScore;
 });
